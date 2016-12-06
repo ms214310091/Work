f2574ea62478e5e169bee1832a4ccd19e3002069
ms214310091 <mitushisindher@gmail.com>
2016-12-06 10:12:47 +0700

    Add files via upload

---
+++ PartOne.js
@@ -0,0 +1,8 @@
+// How to download PhantomJS = phantomjs.org
+// Who know about JS, or PhantomJS...
+
+// Basic about JS
+
+<script type="text/javascript">
+document.write("Today is" + Date());
+</script>
